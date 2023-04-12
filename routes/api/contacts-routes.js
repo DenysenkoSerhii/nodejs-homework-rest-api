133a8346473d6ad6ec72fd7bb453786f6587619b
DenysenkoSerhii <107317701+DenysenkoSerhii@users.noreply.github.com>
2023-04-13 00:55:11 +0300

    1

---
+++ routes/api/contacts-routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const ctrl = require("../../controllers/contacts-controllers");
+
+const { isValidId, authenticate } = require("../../middlewares");
+
+const { validateBody } = require("../../utils");
+
+const { schemas } = require("../../models/contact");
+
+const router = express.Router();
+
+router.get("/", authenticate, ctrl.listContacts);
+
+router.get("/:id", authenticate, isValidId, ctrl.getContactById);
+
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.addContact);
+
+router.put("/:id", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
+
+router.patch("/:id/favorite", authenticate, isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavoriteById);
+
+router.delete("/:id", authenticate, isValidId, ctrl.removeContact);
+
+module.exports = router;
